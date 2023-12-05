97ee2fefcc9847786f11f5da02e3ecb018ba7a98
khaticaa <mirzoevaxedice0@gmail.com>
2023-12-05 15:59:40 +0400

    fetch

---
+++ details.js
@@ -0,0 +1,18 @@
+let id = new URLSearchParams(window.location.search).get("id");
+
+
+
+ let box = document.querySelector(".box")
+
+fetch(`http://localhost:3000/robots/${id}`)
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+
+    box.innerHTML +=`
+     <img src="${data.image}"> 
+     `
+ 
+
+})
+
--- script.js
+++ script.js
@@ -1,39 +1,34 @@
 
 const nav = document.querySelector('nav')
 
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY > 50){
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
         nav.style.background = 'rgba(138, 144, 255, 0.9)'
     }
-    else{
+    else {
         nav.style.background = 'transparent'
     }
 })
 
 
 
-  
+
 
 const boxes = document.querySelector('.s1bottom')
-  
-const producs =[
-    {name:"The Upper Eye", p:"Who are in extremely love with eco friendly system", button: 'view details', ImageSrc:"p1.png.webp"},
-    {name:"The Upper Eye", p:"Who are in extremely love with eco friendly system", button: 'view details', ImageSrc:"p1.png.webp"},
-    {name:"The Upper Eye", p:"Who are in extremely love with eco friendly system", button: 'view details', ImageSrc:"p1.png.webp"},
-    {name:"The Upper Eye", p:"Who are in extremely love with eco friendly system", button: 'view details', ImageSrc:"p1.png.webp"},
-];
+fetch('http://localhost:3000/robots')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            boxes.innerHTML += `
+                <div class="card">
+                <div class="s1img">
+                   <img src="${item.image}" alt="" />
+                </div>
+                <h3>${item.name}</h3>
+                <p>${item.description}</p>
+                <button><a href ="./details.html?id=${item.id}">view details</a></button>
+                </div>
+        `
+        })
+    })
 
-producs.forEach((item) =>{
-    const card = document.createElement('div')
-    card.classList.add('card');
-
-    card.innerHTML = `
-    <div class="s1img">
-               <img src="${item.ImageSrc}" alt="" />
-            </div>
-            <h3>${item.name}</h3>
-            <p>${item.p}</p>
-           <button>${item.button}</button>
-    `;
-    boxes.appendChild(card)
-})
