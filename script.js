79ed45d71d38d99046487ad498e4b1c35679ffa7
khaticaa <mirzoevaxedice0@gmail.com>
2023-12-07 15:35:42 +0400

    load

--- script.js
+++ script.js
@@ -2,24 +2,66 @@
 const nav = document.querySelector('nav')
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-        nav.style.background = 'rgba(138, 144, 255, 0.9)'
-    }
-    else {
-        nav.style.background = 'transparent'
-    }
+  if (window.scrollY > 50) {
+    nav.style.background = 'rgba(138, 144, 255, 0.9)'
+  }
+  else {
+    nav.style.background = 'transparent'
+  }
 })
 
-
-
-
-
 const boxes = document.querySelector('.s1bottom')
-fetch('http://localhost:3000/robots')
+
+function deleteRobot(id) {
+  axios.delete(`http://localhost:3000/robots/${id}`)
+  window.location.reload()
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let nav = document.querySelector("nav");
+  let menuButton = document.getElementById("click");
+  let menu = document.querySelector(".menu");
+
+  menuButton.addEventListener("click", function () {
+    menu.classList.toggle("show-menu");
+  });
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 1000) {
+      menu.classList.remove("show-menu");
+    }
+  });
+});
+
+
+
+let clickTop = document.querySelector("#clickTop");
+
+clickTop.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+
+
+
+
+
+let page = 4
+let loadBtn = document.querySelector("#loadBtn")
+
+function showData() {
+  fetch('http://localhost:3000/robots')
     .then(response => response.json())
     .then(data => {
-        data.forEach(item => {
-            boxes.innerHTML += `
+      data.slice(page - 4, page).forEach(item => {
+        boxes.innerHTML += `
                 <div class="card">
                 <div class="s1img">
                    <img src="${item.image}" alt="" />
@@ -27,71 +69,24 @@ fetch('http://localhost:3000/robots')
                 <h3>${item.name}</h3>
                 <p>${item.description}</p>
                 <button><a href ="./details.html?id=${item.id}">view details</a></button>
-                <button onclick="deleteRobot(${item.id})">Delete</button
+                <button onclick="deleteRobot(${item.id})">Delete</button>
+                <button onclick="updateRobot(${item.id})">Update</button>
                 </div>
         `
-        })
+      })
     })
-
-
-
-function deleteRobot(id){
-    axios.delete(`http://localhost:3000/robots/${id}`)
-    window.location.reload()
 }
 
 
+showData()
 
-document.addEventListener("DOMContentLoaded", function () {
-    let nav = document.querySelector("nav");
-    let menuButton = document.getElementById("click");
-    let menu = document.querySelector(".menu");
-  
-    menuButton.addEventListener("click", function () {
-      menu.classList.toggle("show-menu");
-    });
-  
-    window.addEventListener("resize", function () {
-      if (window.innerWidth > 1000) {
-        menu.classList.remove("show-menu");
-      }
-    });
-  });
-
-
-
-let clickTop= document.querySelector("#clickTop");
-
-clickTop.addEventListener('click', function() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  });
+loadBtn.addEventListener("click" , ()=> {
+  page+=4
+  showData()
+})
 
 
 
 
 
-// const name = document.querySelector("#name")
-// const category = document.querySelector("#category")
-// const image = document.querySelector("#image")
-// const img = document.querySelector("#img")
-// const form = document.querySelector(".category-form")
 
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault()
-//     let obj = {}
-//     let src = image.files[0]
-//     const reader = new FileReader();
-//     reader.onload = function (e) {
-//         obj = {
-//             image: e.target.result,
-//             name: name.value,
-//             category: category.value
-//         }
-//         axios.post("http://localhost:3000/robots", obj)
-//     };
-//     reader.readAsDataURL(src);
-//     window.location="index.html"
-// })
